61d6d67ee42ebbae210aa77729acc353223f20da
Serhii-Chekalov <78024336+Serhii-Chekalov@users.noreply.github.com>
2021-04-02 14:49:41 +0000

    Deploying to gh-pages from @ Serhii-Chekalov/project-HellEN@d10adbf15c11c07c122d37524b22bd265eb381e9 🚀

--- js.324fcdf9.js
+++ js.324fcdf9.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"QvaY":[function(require,module,exports) {
 "use strict";require("../sass/main.scss");
 },{"../sass/main.scss":"clu1"}]},{},["QvaY"], null)
-//# sourceMappingURL=/project-HellEN/js.2f8aa53f.js.map
+//# sourceMappingURL=/project-HellEN/js.324fcdf9.js.map
